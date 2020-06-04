0890f2fb50c5af791925f0b0bb89cc5d46653062
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 18:45:42 +0300

    Create ShopRepository.cs
    added shop repos

---
+++ Project_1/Inventory_conrol_sys/DAL/Repositories/Impl/ShopRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DAL.EF;
+using DAL.Repositories.Interfaces;
+using DAL.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class ShopRepository
+        : BaseRepository<Shop>, IShopRepository
+    {
+        internal ShopRepository(SysteamContext context)
+            : base(context)
+        {
+        }
+    }
+}
