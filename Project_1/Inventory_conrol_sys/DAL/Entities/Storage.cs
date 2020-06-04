84d5d0ad1c063d7cefd0a1ad48de5280ac03489f
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 15:16:59 +0300

    Create Storage.cs
    Created classes in DAL

---
+++ Project_1/Inventory_conrol_sys/DAL/Entities/Storage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Storage
+    {
+        public int storage_id { get; set; }
+        public string storage_address { get; set; }
+        public string item_name { get; set; }
+        public int item_amount { get; set; }
+    }
+}
