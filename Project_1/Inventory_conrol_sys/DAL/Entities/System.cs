8cdac7c4d494b55a49627afa17074bb17468dabf
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 15:16:54 +0300

    Create System.cs
    Created classes in DAL

---
+++ Project_1/Inventory_conrol_sys/DAL/Entities/System.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class System
+    {
+        public int shop_id { get; set; }
+        public int storage_id { get; set; }
+    }
+}
