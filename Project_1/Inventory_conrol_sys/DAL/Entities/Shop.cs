5ba7511fc9f033718664a7e73d42411ef9385769
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 15:17:02 +0300

    Create Shop.cs
    Created classes in DAL

---
+++ Project_1/Inventory_conrol_sys/DAL/Entities/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Shop
+    {
+        public int shop_id { get; set; }
+        public string shop_address { get; set; }
+        public string item_name { get; set; }
+        public int item_amount { get; set; }
+    }
+}
