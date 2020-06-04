f73eaa2759801f563de03eacc5604ab5d2f55ad5
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 18:45:53 +0300

    Create SystemsRepository.cs
    added sys repos

---
+++ Project_1/Inventory_conrol_sys/DAL/Repositories/Impl/SystemsRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using DAL.EF;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class SysteamRepository
+        : BaseRepository<Systeam>, ISysteamRepository
+    {
+
+        internal SysteamRepository(SysteamContext context)
+            : base(context)
+        {
+        }
+    }
+}
