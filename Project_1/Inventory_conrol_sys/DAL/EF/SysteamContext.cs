f862e9f78b8a98c5165e2442568bc2a6e343b5b9
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 18:45:07 +0300

    Create SysteamContext.cs
    added context

---
+++ Project_1/Inventory_conrol_sys/DAL/EF/SysteamContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DAL.Entities;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class SysteamContext
+        : DbContext
+    {
+        public DbSet<Systeam> Phones { get; set; }
+        public DbSet<Shop> Orders { get; set; }
+        public SysteamContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
