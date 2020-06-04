39f4ed122295d54395cb2342bbceb411d091be58
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 18:46:05 +0300

    Create UnitofWork.cs

---
+++ Project_1/Inventory_conrol_sys/DAL/Unit of work/UnitofWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Unit_of_work
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ISysteamRepository Systeams { get; }
+        IShopRepository Shops { get; }
+        void Save();
+    }
+}
