4a8cbebe6793b7352eba60362df0252213d5dcc5
maxl1245 <64740814+maxl1245@users.noreply.github.com>
2020-06-04 18:44:52 +0300

    Create EFUnitofWork.cs
    Added unit of work

---
+++ Project_1/Inventory_conrol_sys/DAL/EF/EFUnitofWork.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using DAL.Unit_of_work;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class EFUnitOfWork
+        : IUnitOfWork
+    {
+        private SysteamContext db;
+        private SysteamRepository systeamRepository;
+        private ShopRepository shopRepository;
+
+        public EFUnitOfWork(SysteamContext context)
+        {
+            db = context;
+        }
+        public ISysteamRepository Systeams
+        {
+            get
+            {
+                if (systeamRepository == null)
+                    systeamRepository = new SysteamRepository(db);
+                return systeamRepository;
+            }
+        }
+
+        public IShopRepository Shops
+        {
+            get
+            {
+                if (shopRepository == null)
+                    shopRepository = new ShopRepository(db);
+                return shopRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
